5fd21ff86f08083ed636cdcb1b7016e16b6a0e38
matheus.guerra <matheusguerra@outlook.com>
2022-03-16 16:18:16 -0300

    tests

--- fm/core/tests.py
+++ fm/core/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeTeste(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """GET / must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """GET / must return template index.html"""
+        self.assertTemplateUsed(self.response, 'index.html')
