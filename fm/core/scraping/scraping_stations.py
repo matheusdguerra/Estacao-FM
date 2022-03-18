1fcdffa9a39f5cc4a6be22ed630c9773a9ba8ad0
matheus.guerra <matheusguerra@outlook.com>
2022-03-18 20:49:55 -0300

    scraping load dados

--- fm/core/scraping/scraping_stations.py
+++ fm/core/scraping/scraping_stations.py
@@ -6,7 +6,13 @@
 
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
+data = []
+radio = []
+
+# payload com todos os estado e municipios
+"""
 payload = {
     'acao': 'p',
     'PagSRD': '/SRD/RELATORIOS/RADIODIFUSAO/RELRADIOCOMPLETO.ASP',
@@ -18,10 +24,27 @@ payload = {
     'SiglaUF': 'TT',
     'CodMunicipio': 'TT',
     'radioSituacao': '0'
+    }
+"""
+
+# payload RS Porto Alegre
+
+payload = {
+    'acao': 'p',
+    'PagSRD': '/SRD/RELATORIOS/RADIODIFUSAO/RELRADIOCOMPLETO.ASP',
+    'strAction': '/SRD/Relatorios/Radiodifusao/RelRadioCompleto.asp',
+    'vIndMunicipioTodos': 'True',
+    'vIndUFTodos': 'True',
+    'OP': '5',
+    'NumServico': '230',
+    'SiglaUF': 'RS',
+    'CodMunicipio': '4314902',
+    'radioSituacao': '0'
 }
 
 response = requests.get('https://sistemas.anatel.gov.br/SRD/Relatorios/Radiodifusao/RelRadioCompleto.asp',
                         params=payload)
+
 
 content = response.content
 
@@ -29,50 +52,52 @@ site = BeautifulSoup(content, 'html.parser')
 
 stations_full = site.find('form', attrs={'method': 'post'})
 
-stations = stations_full.find('tbody')
 
-print(stations)
+for table in stations_full.findAll('table', attrs={'class': 'Tabela'}):
+    for line in table.findAll('tr'):
+        for rows in line.findAll('td', attrs={'class': 'CampoEsquerda'}):
 
+            radio.append(rows.text)
 
-# for station in stations:
-#    station_name = station.find('td', attrs={'class': 'CampoEsquerda'})
-#    print(station_name.text)
+inicio = 2
+final = 25  # cada rádio tem 24 informações
 
+for idx, c in enumerate(radio):
+    if idx > len(radio)/25:
+        break
+    else:
+        data.append(radio[inicio:final])
+        inicio = final
+        final += 23
+# print(data)
 
+dataframe = pd.DataFrame(data, colluns=['Nome_Entidade', 'uf', 'localidade', 'frequencia'])
+
+print(dataframe)
+"""
+for lista in data:
+    if lista[1].replace(u'\xa0', u'') == '':
+        entidade = lista[0].strip()
+    else:
+        entidade = lista[1].strip()
+
+    uf = lista[16].strip()
+
+    localidade = lista[15].strip()
+
+    frequencia = lista[20].strip()
+
+    print('Entidade:', entidade)
+    print('UF:', uf)
+    print('Localidade:', localidade)
+    print('Frequencia:', frequencia)
+    print('')
 """
 
-payload = {
-    'Servico': '230',
-    'acao': 'listagem',
-    'op': '5',
-    'SiglaServico': 'FM',
-    'Situacao': '1',
-    'NumServico': '230',
-    'SiglaUF': 'RS'
-}
-
-response = requests.get('https://sistemas.anatel.gov.br/srd/Consultas/ConsultaGeral/TelaListagem.asp',
-                        params=payload)
-
-content = response.content
-
-site = BeautifulSoup(content, 'html.parser')
-
-stations = site.findAll('tr', attrs={'id': 'TRplus'})
-
-for station in stations:
-    station_name = station.find('td', attrs={'class': 'CampoEsquerda'})
-    print(station_name.text)
-
-<td class = "CampoEsquerda" width = "*%" > RADIO MINUANO DE ALEGRETE LTDA < /td >
-
-<td class = "CampoCentro" width = "*%" > RS < /td >
-
-<td class = "CampoEsquerda" width = "*%" > Alegrete < /td >
-
-<a href = "javascript:Redir('230', 'FM    ', 'RS', '4300406', '33806', '', '45347', '97,5 MHz', '3 ', '')" > 248 < /a >
-
-station_name = station.find('td', attrs={'class': 'CampoEsquerda'})
-print(station_name.text)
-
+"""
+'Nome Entidade', 'Nome Fantasia', 'CNPJ',	'Nº Fistel',	'Classe', 'Nº Estação', 'End. Entidade', 'Cidade', 'UF'	 RS	CEP:	 90840440
+End. Corr.:	 RUA ORFANATROFIO, 711 - ALTO PETROPOLIS, .	Cidade:	 Porto Alegre	UF:	 RS	CEP:	 90840440
+End. Estação:	 RUA ORFANATROFIO
+711, .	Cidade:	 Porto Alegre	UF:	 RS	CEP:	 90000000
+Latitude:	 30S045200	Longitude:	 51W105900	Freqüência:	 98, 3 MHz	Prefixo:	 ZYD676	Potência:	 10.000 kW
 """
