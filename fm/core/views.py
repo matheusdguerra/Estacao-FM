29be5f458e6b5242b7c29caa29c1893d83530751
matheus.guerra <matheusguerra@outlook.com>
2022-04-05 15:51:35 -0300

    station uf

--- fm/core/views.py
+++ fm/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from fm.core.models import Stations
 from .filters import StationFilter
 
@@ -6,11 +6,13 @@ from .filters import StationFilter
 def home(request):
     template_name = 'index.html'
     stations_list = Stations.objects.all()
+    uf_list = Stations.objects.values('uf').order_by('uf').distinct()
     stations_filter = StationFilter(request.GET, queryset=stations_list)
 
     context = {
         'stations_list': stations_list,
-        'filter': stations_filter
+        'filter': stations_filter,
+        'uf_list': uf_list
     }
 
     return render(request, template_name, context)
@@ -19,3 +21,8 @@ def home(request):
 def station_detail(request, id):
     station = get_object_or_404(Stations, pk=id)
     return render(request, 'detail.html', {'station': station})
+
+
+def station_uf(request, uf):
+    stations_uf = get_list_or_404(Stations, uf=uf)
+    return render(request, 'station_uf.html', {'stations_uf': stations_uf})
--- fm/urls.py
+++ fm/urls.py
@@ -1,18 +1,3 @@
-"""fm URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from fm.core import views
@@ -21,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('<int:id>/', views.station_detail, name='station_detail'),
+    path('<str:uf>/', views.station_uf, name='station_uf'),
 ]
